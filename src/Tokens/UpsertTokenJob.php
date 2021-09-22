ed33de07fbe81bbe5a876fe19a2c69250e2a8489
Bruno Falcão <me@brunofalcao.dev>
2021-09-22 21:19:03 +0200

    Update UpsertTokenJob.php

--- src/Tokens/UpsertTokenJob.php
+++ src/Tokens/UpsertTokenJob.php
@@ -42,22 +42,40 @@ class UpsertTokenJob implements ShouldQueue
     {
         // Insert token into the database and update pairs if needed.
         DB::transaction(function () {
+            /**
+             * Remove the 'quote' column from the array so it doesn't blow up
+             * the Token updateOrCreate().
+             */
             $quote = null;
             if (array_key_exists('quote', $this->token)) {
                 $quote = $this->token['quote'];
                 unset($this->token['quote']);
             }
 
+            /**
+             * The token is now added or updated. What matters also is that
+             * only the keys that were passed are the ones used, so the other
+             * columns are not affected with NULL values.
+             */
             $token = Token::updateOrCreate(
                 ['symbol' => $this->token['symbol']],
                 $this->token
             );
 
+            /**
+             * If we have a quote, then lets update/insert it into the
+             * pairs table. This is useful to then cross-match with the
+             * orders and alerts. Also, each token will create as much pairs
+             * as the stablecoins that they are connected to, no matter
+             * what exchange is being used.
+             */
             if ($quote) {
-                if (!Pair::firstWhere('token_id')) {
+                if (!Pair::where('token_id', $token->id)
+                         ->where('quote', $quote)
+                         ->first()) {
                     Pair::create(
-                        ['token_id' => $token->id],
-                        ['quote'    => $quote]
+                        ['token_id' => $token->id,
+                         'quote'    => $quote]
                     );
                 }
             }
